d4047d9330a5f83fdb7356200eb0aa79eac9f1ba
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-06 14:17:09 -0600

    publisher validation from data

--- src/heroes/data/heroes.js
+++ src/heroes/data/heroes.js
@@ -14,5 +14,53 @@ export const heroes = [
         'alter_ego': 'Kal-El',
         'first_appearance': 'Action Comics #1',
         'characters':'Kal-El'
+    },
+    {
+        'id':'marvel-barton',
+        'superhero': 'Ojo de alcon',
+        'publisher': 'Marvel Comics',
+        'alter_ego':'Clinton Francis Barton',
+        'first_appearance': 'Tales of Suspense #57',
+        'characters': 'Clinton Francis Barton'
+    },
+    {
+        'id':'marvel-spider',
+        'superhero': 'Spider Man',
+        'publisher': 'Marvel Comics',
+        'alter_ego': 'Peter Parker',
+        'first_appearance': 'Amazing Fantasy #15',
+        'characters': 'Peter Parker'
+    },
+    {
+        'id': 'marvel-captain',
+        'superhero': 'Captain America',
+        'publisher': 'Marvel Comics',
+        'alter_ego': 'Steve Rogers',
+        'first_appearance': 'Captain America Comics #1',
+        'characters': 'Steve Rogers'
+    },
+    {
+        'id': 'marvel-iron',
+        'superhero': 'Iron Man',
+        'publisher': 'Marvel Comics',
+        'alter_ego': 'Tony Stark',
+        'first_appearance': 'Tales of Suspense #39',
+        'characters': 'Tony Stark'
+    },
+    {
+        'id': 'marvel-cyclops',
+        'superhero': 'Cyclops',
+        'publisher': 'Marvel Comics',
+        'alter_ego':'Scott Summers',
+        'first_appearance': 'X-Men #1',
+        'characters': 'Scott Summers'
+    },
+    {
+        'id': 'marvel-silver',
+        'superhero': 'Silver Surfer',
+        'publisher': 'Marvel Comics',
+        'alter_ego': 'Norrin Radd',
+        'first_appearance': 'The Fantastic Four #48',
+        'characters': 'Norrin Radd'
     }
 ]
