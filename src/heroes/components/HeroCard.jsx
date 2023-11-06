fb359e9a748fb984856327b7b3db9ce776db3be5
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-06 15:59:39 -0600

    Update HeroCard.jsx

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -9,6 +9,14 @@ import { Button, Card,
     Stack, 
     Text } from '@chakra-ui/react';
 
+const CharactersByHero = ({ alter_ego, characters}) => {
+    // if(alter_ego !== characters) return (<></>);
+    // return <Text> { characters } </Text>;
+    return (alter_ego !== characters)
+    ?<></>
+    :<Text> { characters } </Text>;
+}
+
 export const HeroCard = ({
     id,
     superhero,
@@ -30,9 +38,7 @@ export const HeroCard = ({
             />
             <Stack mt='6' spacing='3'>
                     <Heading size='md'>{ superhero}</Heading>
-                    {
-                        (alter_ego !== characters) && (<Text> { characters } </Text>)
-                    }
+                    <CharactersByHero characters={ characters } alter_ego={alter_ego} />
                     <Text color='blue.600' fontSize='2xl'>
                         {alter_ego}
                     </Text>
@@ -41,7 +47,7 @@ export const HeroCard = ({
         </CardBody> 
         <Divider />
         <CardFooter>
-            <Link to={`/hero`}>
+            <Link to={`/hero/${ id }`}>
             <Button variant='solid' colorScheme='blue'>Mas..</Button>
             </Link>
         </CardFooter>
