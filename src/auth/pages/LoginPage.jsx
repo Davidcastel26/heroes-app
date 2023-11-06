16051f2d24c1610d92f6c761df17216b259b6140
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-06 13:34:39 -0600

    creating and fixing login ui

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,6 +1,33 @@
+import { Button, Text } from "@chakra-ui/react"
+import { useNavigate } from "react-router-dom"
+import ToggleColorMode from "../../ui/btnColorMode"
 
 export const LoginPage = () => {
+
+  const navigate = useNavigate()
+
+  const onLogin = () => {
+    navigate('/marvel',{
+      replace: true,
+    });
+  }
+
   return (
-    <h1>LoginPage</h1>
+    <div>
+      <div className="flex justify-between mb-4">
+        <Text as="h1">
+          Login
+        </Text>
+
+        <ToggleColorMode />
+      </div>
+      <hr />
+      <Button
+        className="mt-3"
+        onClick={ onLogin }
+      >
+        Login
+      </Button>
+    </div>
   )
 }
--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,65 +1,88 @@
-import React from 'react'
-import ToggleColorMode from './btnColorMode'
-import { Button } from '@chakra-ui/react'
-import { Link, NavLink } from 'react-router-dom'
-// import ToggleColorMode from '../heroes/components/ui/btnColorMode'
-// import ToggleColorMode from './ui/btnColorMode'
+import React from "react";
+import ToggleColorMode from "./btnColorMode";
+import { Button } from "@chakra-ui/react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
 
 export const Navbar = () => {
+
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    navigate('/login',{
+      replace:true
+    });
+  };
+
   return (
-
-<nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link to="/">
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-        Asociaciones
-      </span>
-    </Link>
-    <div className="flex md:order-2">
-         
-        <Button className=''>
-          <NavLink to="login">
-          Login
-          </NavLink>
-        </Button>
-        <ToggleColorMode />   
-        <button 
-          data-collapse-toggle="navbar-sticky" 
-          type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" 
-          aria-expanded="false">
-          <span className="sr-only">Open main menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h15M1 7h15M1 13h15"/>
-          </svg>
-      </button>
-
-    </div>
-  <div 
-    className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" 
-    id="navbar-sticky">
-    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-      
-      <li>
-        <NavLink
-          to="/marvel"
-          className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+    <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Asociaciones
+          </span>
+        </Link>
+        <div className="flex md:order-2">
+          <Button 
+            onClick={ onLogout }
+          >
+            Logout
+          </Button>
+          <ToggleColorMode />
+          <button
+            data-collapse-toggle="navbar-sticky"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-sticky"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
         >
-        {/* <a href="#" class=""> */}
-          Marvel
-          {/* </a> */}
-        </NavLink>
-      </li>
-      <li>
-       <NavLink
-          to="/dc"
-          className={ ({isActive})=> `block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${isActive ? 'text-blue-700': ''}`}>
-          DC
-        </NavLink>
-      </li>
-     
-    </ul>
-  </div>
-  </div>
-</nav>
-  )
-}
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <NavLink
+                to="/marvel"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                {/* <a href="#" class=""> */}
+                Marvel
+                {/* </a> */}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/dc"
+                className={({ isActive }) =>
+                  `block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700 ${
+                    isActive ? "text-blue-700" : ""
+                  }`
+                }
+              >
+                DC
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
