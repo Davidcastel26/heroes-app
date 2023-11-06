709668dff8f8a5f7c1b65fa0b8d004257acf447b
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-06 16:34:28 -0600

    Hero page with params

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,5 +1,17 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { getHeroesById } from '../helpers';
 
 export const HeroPage = () => {
+
+  const { id } = useParams();
+  // console.log(id)
+  const hero = getHeroesById( id );
+  console.log(hero);
+
+  if( !hero ){
+    return <Navigate to="/marvel" />
+  }
+
   return (
     <h1>HeroPage</h1>
   )
