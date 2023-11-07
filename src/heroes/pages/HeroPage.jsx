c14fc1ead2d8f1111c3d632f59cf763534d57554
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-06 18:55:16 -0600

    Update HeroPage.jsx

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,18 +1,39 @@
-import { Navigate, useParams } from 'react-router-dom';
-import { getHeroesById } from '../helpers';
+import { Navigate, useParams } from "react-router-dom";
+import { getHeroesById } from "../helpers";
+import { Image, Text } from "@chakra-ui/react";
 
 export const HeroPage = () => {
-
   const { id } = useParams();
   // console.log(id)
-  const hero = getHeroesById( id );
+  const hero = getHeroesById(id);
   console.log(hero);
 
-  if( !hero ){
-    return <Navigate to="/marvel" />
+  if (!hero) {
+    return <Navigate to="/marvel" />;
   }
 
   return (
-    <h1>HeroPage</h1>
-  )
-}
+    <section>
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <Text fontSize='6xl' className="font-extrabold tracking-tight "> 
+          {/* // className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white"> */}
+          { hero.superhero }
+          </Text>
+          <Text fontSize='1xl' className="max-w-2xl mb-6 font-light ">
+            Alter ego: { hero.alter_ego }
+          </Text>
+          <p className="max-w-2xl mb-6 font-light">
+            Publisher: { hero.publisher }
+          </p>
+          
+
+        
+        </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <Image src={`/public/assets/heroes/${id}.jpg`} alt={hero.superhero} />
+        </div>
+      </div>
+    </section>
+  );
+};
