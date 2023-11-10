876c39c5d8cf2a8ea540b9d14abab9b93cb1a8e3
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-09 21:38:20 -0600

    search component ui completed

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,91 @@
+import { useLocation, useNavigate } from 'react-router-dom'
+import queryString from 'query-string'
+
+import { SearchIcon } from '@chakra-ui/icons'
+import { Alert, 
+        AlertDescription, 
+        AlertIcon,
+        AlertTitle, 
+        Button, 
+        Divider, 
+        FormLabel, 
+        Input, 
+        Text } from '@chakra-ui/react'
+
+import { useForm } from '../../hooks/useForm'
+import {HeroCard} from '../components'
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  // console.log({ location })
+  const {q = ''} = queryString.parse( location.search )
+  // console.log({query})
+
+  const { searchText, onInputChange } = useForm({
+    searchText:''
+  })
+
+  const onSearchSubmit = ( event ) => {
+    event.preventDefault();
+    if( searchText.trim().length <= 1 ) return;
+    
+    navigate(`?q=${ searchText.toLowerCase().trim() }`)
+
+  }
+
   return (
-    <h1>SearchPage</h1>
+    <>
+      <Text fontSize='5xl'>Search</Text>
+      <Divider />
+
+      <div className="grid grid-cols-2 m-5">
+
+      <div className="col-span-1">
+        <FormLabel>Searching</FormLabel>
+        <form onSubmit={ onSearchSubmit }>
+          <Input
+            htmlSize={20} 
+            width='auto' 
+            name='searchText'
+            type="text" 
+            placeholder="Search a hero"
+            className="form-control"
+            autoComplete="off"
+            value={ searchText }
+            onChange={ onInputChange }
+          />
+          <Button
+            className="ml-2"
+            colorScheme='teal'
+            variant='solid'
+            leftIcon={< SearchIcon/>}
+          >
+            Search
+          </Button>
+        </form>
+      </div>
+
+      <div className="col-span-1">
+        <h4>Results</h4>
+        <Divider />
+
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle>Nothing here!</AlertTitle>
+        </Alert>
+
+        <Alert status='warning'>
+            <AlertIcon />
+            No hero with,  <b> { q }</b>
+        </Alert>
+
+        {/* <HeroCard /> */}
+
+      </div>
+
+      </div>
+    </>
   )
 }
