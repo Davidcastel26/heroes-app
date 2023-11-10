02446c093b5716bec54229add6a3ca2daf0e0f06
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2023-11-10 15:03:16 -0600

    creating shearch and working succesffuly

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -14,22 +14,29 @@ import { Alert,
 
 import { useForm } from '../../hooks/useForm'
 import {HeroCard} from '../components'
+import { getHeroesByName } from '../helpers'
 
 export const SearchPage = () => {
 
   const navigate = useNavigate();
   const location = useLocation();
   // console.log({ location })
-  const {q = ''} = queryString.parse( location.search )
+  const {q = ''} = queryString.parse( location.search );
   // console.log({query})
+  const heroes = getHeroesByName(q);
+
+  const showSearch = (q.length === 0) 
+  const showError = (q.length > 0) && heroes.length === 0
 
   const { searchText, onInputChange } = useForm({
-    searchText:''
+    searchText:q
   })
+
+  
 
   const onSearchSubmit = ( event ) => {
     event.preventDefault();
-    if( searchText.trim().length <= 1 ) return;
+    // if( searchText.trim().length <= 1 ) return;
     
     navigate(`?q=${ searchText.toLowerCase().trim() }`)
 
@@ -70,18 +77,37 @@ export const SearchPage = () => {
       <div className="col-span-1">
         <h4>Results</h4>
         <Divider />
-
-        <Alert status='error'>
+        {/* {
+          ( q === '')
+            ? <Alert status='warning'>
+                <AlertIcon />
+                <AlertTitle>Nothing here!</AlertTitle>
+              </Alert>
+          : ( heroes.length === 0 ) && <Alert status='error'> <AlertIcon /> No hero with,  <b> { q }</b> </Alert>
+        } */}
+        <Alert 
+          status='warning' 
+          className='mt-4 animate__animated animate__fadeInRight' 
+          style={{ display: showSearch ? '': 'none'}}>
           <AlertIcon />
           <AlertTitle>Nothing here!</AlertTitle>
         </Alert>
 
-        <Alert status='warning'>
-            <AlertIcon />
-            No hero with,  <b> { q }</b>
+        <Alert 
+          status='error' 
+          className='mt-4 animate__animated animate__fadeInRight' 
+          style={{ display: showError ? '': 'none'}}>
+          <AlertIcon /> 
+          No hero with,  <b> { q }</b> 
         </Alert>
+        
+        
 
-        {/* <HeroCard /> */}
+        {
+          heroes.map(hero => (
+            <HeroCard key={hero.id} {...hero}/>
+          ))
+        }
 
       </div>
 
